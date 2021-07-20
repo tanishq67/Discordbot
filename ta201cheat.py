b1db45afc2fc3b3d3be6a8db284bc67156ab39ac
tanishq67 <52757132+tanishq67@users.noreply.github.com>
2021-07-20 18:25:08 +0530

    Update ta201cheat.py

--- ta201cheat.py
+++ ta201cheat.py
@@ -2,23 +2,25 @@ import discord
 import pyautogui
 import keyboard  # using module keyboard
 
-# client = discord.Client()
+const_Api = '756337173724266657'
+const_Chan = 756211540495040583
+client = discord.Client()
 
-# @client.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'.format(client))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-# def ss():
-# 	screenshot = pyautogui.screenshot()
-# 	screenshot.save("screen.png")
+def ss():
+	screenshot = pyautogui.screenshot()
+	screenshot.save("screen.png")
 
-# async def execute():
-# 	channel = client.get_channel(756211540495040583)
-# 	ss()
-# 	await channel.send(file=discord.File('screen.png'))
-# 	await channel.send('?poll test run "a" "b" "c" "d"')
+async def execute():
+	channel = client.get_channel(const_Chan)
+	ss()
+	await channel.send(file=discord.File('screen.png'))
+	await channel.send('?poll test run "a" "b" "c" "d"')
 
-# client.run('756337173724266657')
+client.run(const_Api)   # const is my api you own api
 
 while True:  # making a loop
     try:  # used try so that if user pressed other than the given key error will not be shown
